8d4dfedd91803691cae916cf62485f0ed903914a
Daisy Dai <893487287@qq.com>
2019-08-08 15:07:20 +0800

    Try to configure the selector to be class and style this class in external css file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue
-    }
-    [app-servers] {
-      border: 1px solid yellow;
-    }
-  `]
+  styleUrls: ['./app.component.css'],
+  // styles: [`
+  //   h3 {
+  //     color: dodgerblue
+  //   }
+  //   [app-servers] {
+  //     border: 1px solid yellow;
+  //   }
+  // `]
 })
 export class AppComponent {
 }
